84514bace0255e7af3118288171d14091ab6bd15
WalaceCR <Cavaleiro88@@>
2024-04-06 10:23:46 -0300

    Arise Kali !!!

---
+++ src/main/java/aulas/aula1/ServicoDeImpressao.java
@@ -0,0 +1,37 @@
+package aulas.aula1;
+
+import java.util.ArrayList;
+
+public class ServicoDeImpressao {
+    private ArrayList<Integer> listaDeNumeros = new ArrayList<>();
+
+    public void add(int valor){
+        listaDeNumeros.add(valor);
+
+    }
+
+    public Object primeiro(){
+        if (listaDeNumeros.isEmpty())
+            //TODO PROGRAMAÇÃO DEFENSIVA - ENTENDER MAIS
+            throw new IllegalStateException("Lista vazia");
+
+        return listaDeNumeros.getFirst();
+    }
+
+    public Object ultimo(){
+        if (listaDeNumeros.isEmpty())
+            throw new IllegalStateException("Lista vazia");
+        return listaDeNumeros.getLast();
+    }
+
+    public void imprimir(){
+        listaDeNumeros.forEach(numero -> {
+            System.out.println("Numero: " + numero);
+        });
+
+        System.out.println("Todos na sequencia: \n" + listaDeNumeros );
+    }
+
+
+
+}
